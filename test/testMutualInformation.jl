03c8982e3aad40e2fc6366e5773049440d7dee9a
Thalia Chan <thalia.e.chan@googlemail.com>
2016-03-21 10:17:25 +0000

    Remove mutual information test

--- test/testMutualInformation.jl
+++ test/testMutualInformation.jl
@@ -4,11 +4,6 @@
 # targets by default, whereas the mutual information formula overcomes
 # by estimating the probabilities of the 3D distribution, then projecting)
 
-# Test mutual information between two identical arrays is the same as the
-# entropy of one of them
-@test get_mutual_information(arr1, arr1) == get_entropy(arr1)
-println("Mutual information of 2 identical arrays passed.")
-
 # Test mutual information formula
 @test get_mutual_information(arr1, arr2) == get_entropy(arr1) + get_entropy(arr2) - get_entropy(arr1, arr2)
 println("Mutual information formula passed.")
