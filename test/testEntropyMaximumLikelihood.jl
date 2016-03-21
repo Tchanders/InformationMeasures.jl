4d6e87fcf6f261e3c2cbb39adbc3954c64a35127
Thalia Chan <thalia.e.chan@googlemail.com>
2016-03-21 10:04:40 +0000

    Remove old files (remove Knn entirely)

--- test/testEntropyMaximumLikelihood.jl
+++
@@ -1,5 +0,0 @@
-# Check entropy is log_base(b) for uniform distribution with b bins
-@test_approx_eq_eps getentropymaximumlikelihood(arr) log2(b) 0.5
-@test_approx_eq_eps getentropymaximumlikelihood(arr, e) log(b) 0.5
-
-println("Maximum likelihood entropy passed")
--- test/testEntropyMillerMadow.jl
+++
@@ -1,5 +0,0 @@
-# Check entropy is log_base(b) for uniform distribution with b bins
-@test_approx_eq_eps getentropymillermadow(arr) log2(b) 0.5
-@test_approx_eq_eps getentropymillermadow(arr, e) log(b) 0.5
-
-println("Miller Madow entropy passed")
