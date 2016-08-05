864d0b2f3568d905f394c87c87f5521ae5f1d50b
Thalia <thalia.e.chan@googlemail.com>
2016-08-04 17:12:17 -0700

    Remove include for non-existent file

--- src/EntropyEstimators.jl
+++ src/EntropyEstimators.jl
@@ -1,9 +1,5 @@
 module EntropyEstimators
 
-# The Bayesian blocks function should be incorporated into Discretizers.jl
-# For now it is inluded here separately
-include("../src/BayesianBlocks.jl")
-
 include("../src/Discretization.jl")
 include("../src/Estimators.jl")
 include("../src/Formulae.jl")
